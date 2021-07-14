90966d22299d83a06c6de62cdcacd7b37d1ee7f4
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-07-13 21:42:38 -0400

    Adding stub projects.

---
+++ src/RainClik.Implementation/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RainClik.Implementation
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/RainClik.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RainClik.Infrastructure
+{
+    public class Class1
+    {
+    }
+}

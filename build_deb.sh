d8096e2ee17fb9f3001dc3b5dc44064c66307a20
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2021-10-02 21:49:34 -0400

    Adding permission change script.

--- build_deb.sh
+++ build_deb.sh
@@ -3,7 +3,6 @@ GITHUB_REF="refs/tags/0.0.1-rc1"
 VERSION="$(echo $GITHUB_REF | sed -E 's,^refs/tags/,,')"
 VERSION="$(echo $VERSION | sed -E 's/^v//')"
 
-chmod +x usr/bin/dotnet-veml7700/WebApp
 chmod -R 0755 dpkg-build/DEBIAN
 
 sed -i 's/${VERSION}/'$VERSION'/g' 'dpkg-build/DEBIAN/control'

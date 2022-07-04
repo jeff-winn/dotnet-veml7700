0f58f175efbe7a02f9f4a1d8fd77e688c7b1d173
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2022-07-04 16:22:12 -0400

    Adding flag to enable the test i2c bus driver.

--- src/WebApp/Infrastructure/ServiceCollectionBootstrapper.cs
+++ src/WebApp/Infrastructure/ServiceCollectionBootstrapper.cs
@@ -4,20 +4,31 @@ using Microsoft.Extensions.Configuration;
 using WebApp.Infrastructure.Factories;
 using WebApp.Services;
 
-namespace Microsoft.Extensions.DependencyInjection {
-	static class ServiceCollectionBootstrapper {
-		public static IServiceCollection AddWebAppServices(this IServiceCollection services, IConfiguration configuration) 
-		{		
+namespace Microsoft.Extensions.DependencyInjection
+{
+	static class ServiceCollectionBootstrapper
+	{
+		public static IServiceCollection AddWebAppServices(this IServiceCollection services, IConfiguration configuration)
+		{
 			services.AddTransient<IInspectionService, InspectionService>();
-			
-			services.AddSingleton<II2cBus>(sp => {
+
+			services.AddSingleton<II2cBus>(sp =>
+			{
 				var options = configuration.GetVeml7700Options();
 
 				return new I2cBusWrapper(
 					I2cBus.Create(options.BusId));
 			});
 
-			services.AddSingleton<IVeml7700DriverFactory, Veml7700DriverFactory>();
+			if (configuration.GetValue<bool>("UseFakeI2cBus"))
+			{
+				services.AddSingleton<IVeml7700DriverFactory, FakeVeml7700DriverFactory>();
+			}
+			else
+			{
+				services.AddSingleton<IVeml7700DriverFactory, Veml7700DriverFactory>();
+			}
+
 			return services;
 		}
 	}
